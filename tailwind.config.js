33ac894cacf6e8f9e0ad6ecf113ccf8be8f64c22
Cristiano Abilio <cristianocafr@gmail.com>
2026-03-16 11:12:54 -0300

    feat: result page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './app/Enums/*.php'
     ],
 
     theme: {
@@ -33,7 +34,7 @@ export default {
                     '900': '#0e1117',
                     '800': '#282a36',
                 }
-            },
+            }
         },
     },
 
